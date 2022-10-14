e7d64fb62d2be9411a763f90eec7e5b03a25d4e4
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-10-14 13:51:31 -0300

    CRUD scaffolding

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,7 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
 
 var app = builder.Build();
 
